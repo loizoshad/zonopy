Fixed Computation of FRS of non-ego vehicles

src/main.py
@@ -1,90 +1,152 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import time
-import copy
 
-from utils.environments.dynamic_env2 import DynamicEnv2
+from utils.environments.dynamic_env import DynamicEnv, StateSpaceSafe, DynamicObstacleSpace
+from utils.sets.hybrid_zonotopes import HybridZonotope
+from utils.sets.constrained_zonotopes import ConstrainedZonotope
 from utils.visualization import ZonoVisualizer
 from utils.operations.operations import ZonoOperations
-from utils.ego_model_2d import DynamicsModel
+from utils.ego_model_4d import DynamicsModel
 
+np.set_printoptions(edgeitems=10, linewidth=10000)
 colors = [
     (0.949, 0.262, 0.227, 0.6),     # Obstacle (Red)
     (0.717, 0.694, 0.682, 0.5),     # Road (Gray)
     (0.231, 0.780, 0.160, 1.0),     # Parking spot (Green)
     (0.423, 0.556, 0.749, 0.5)      # BRS (Blue)
 ]
+obs_color = (0.949, 0.262, 0.227, 0.6)
 
-
-np.set_printoptions(edgeitems=10, linewidth=10000)
 marker_size = 0.5 * 39.36           # 0.2m in inches
 marker_size = marker_size**2        # area of the marker
 first_time = True
 
+
+def get_obstacle(obs: HybridZonotope):
+    '''
+    This method takes in the forward reachable set of an obstacle and it then computes its over-approximation as a hypercube.
+    In addition this over-approximation is used to make sure that the ego vehicle does not collide with the obstacle,
+    cause it assumes that at that time step the object is static at that position.
+    '''
+    # Extract position states
+    obs_pos = HybridZonotope(obs.Gc[0:2, :], obs.Gb[0:2, :], obs.C[0:2, :], obs.Ac, obs.Ab, obs.b)
+    cz = zono_op.oa_hz_to_cz(obs)
+    # cz = zono_op.redundant_c_g_cz(cz)
+    cz = zono_op.oa_cz_to_hypercube_tight_4d(cz)
+
+
+    # Set the velocity space to the full velocity space
+    G = np.array([
+        [cz.G[0, 0],    0.0    , 0.0, 0.0],
+        [   0.0    , cz.G[1, 1], 0.0, 0.0],
+        [   0.0    ,    0.0    , 1.0, 0.0],
+        [   0.0    ,    0.0    , 0.0, 1.0]
+    ])
+    C = np.array([
+        [cz.C[0, 0]],
+        [cz.C[1, 0]],
+        [0.0],
+        [0.0]
+    ])
+    hz = HybridZonotope(G, np.zeros((cz.dim, 0)), C, np.zeros((0, cz.dim)), np.zeros((0, 0)), np.zeros((0, 1)) )
+    hz = zono_op.intersection_hz_hz(hz, space)
+
+    return hz
+
+
+
+
+
 options = 'outer'
 reduction_options = 'reduced'
+
+road = 'down_right_v4'
+print(f'****************************************************************************************************')
+print(f'                                          Settings                                                  ')
+print(f'****************************************************************************************************')
+print(f'Roads included : {road}')
 
 # Initialize objects
 dynamics = DynamicsModel()
 zono_op = ZonoOperations()
 vis = ZonoVisualizer(zono_op = zono_op)
-env = DynamicEnv2(zono_op = zono_op, dynamics = dynamics, visualizer = vis, options = options)
+env = DynamicEnv(zono_op = zono_op, dynamics = dynamics, visualizer = vis, options = options)
 
 # Create the space
-space = env.state_space             # Safe state space
+# space = env.state_space             # Safe state space
 target = env.target_space           # Free parking spots
 input = env.input_space
 obs = env.initial_space   # Initial space of non-ego vehicles
 
-print(f'obs.C = {obs.C.T}')
-print(f'obs.Gc = \n{obs.Gc}')
-print(f'obs.Gb = \n{obs.Gb}')
-print(f'obs.Ac = \n{obs.Ac}')
-print(f'obs.Ab = \n{obs.Ab}')
-print(f'obs.b = {obs.b.T}')
+road_down = StateSpaceSafe().road_down
+road_right = StateSpaceSafe().road_right
+space = zono_op.union_hz_hz_v2(road_down, road_right)
+
+print(f'****************************************************************************************************')
+print(f'                                          FRS SETS                                                  ')
+print(f'****************************************************************************************************')
+
+# Initialize obstacle based on region it is in
+car_down = zono_op.intersection_hz_hz(obs, road_down)
+car_right = zono_op.intersection_hz_hz(obs, road_right)
 
 
-N = 11
+N = 101
 for i in range(N):
     print(f'*********************************************************')
     print(f'Iteration {i}')
 
-    # Compute state of dynamic obstacle
-    # obs = zono_op.one_step_frs_hz(X = space, U = input, I = obs, A = env.A, B = env.B, W = env.W)
-    obs = zono_op.one_step_brs_hz_v2(X = space, U = input, T = obs, A = env.A, B = env.B)
-    # obs = zono_op.intersection_hz_hz(obs, space)
 
-    # Over-approximate obstacle
-    cz = zono_op.oa_hz_to_cz(obs)
-    cz = zono_op.redundant_c_g_cz(cz)
-    cz = zono_op.oa_cz_to_hypercube_tight(cz)
-    obs = zono_op.cz_to_hz(cz)
-    obs = zono_op.intersection_hz_hz(obs, space)
+    car_down_1 = zono_op.one_step_frs_hz(X = road_down, U = input, I = car_down, A = env.A, B = env.B, W = env.W)     # Car down  # V3
+    #
+    car_right_1 = zono_op.one_step_frs_hz(X = road_right, U = input, I = car_right, A = env.A, B = env.B, W = env.W)  # Car right # V3
+    car_right_2 = zono_op.one_step_frs_hz(X = road_right, U = input, I = car_down, A = env.A, B = env.B, W = env.W)  # Car right # V3
 
-    # Compute safe space (complement of obstaccle)
-    safe = zono_op.complement_cz_to_hz(cz)
-    safe = zono_op.intersection_hz_hz(safe, space)
 
-    # print(f'obs.C = {obs.C.T}')
-    # print(f'obs.Gc = \n{obs.Gc}')
-    # print(f'obs.Gb = \n{obs.Gb}')
-    # print(f'obs.Ac = \n{obs.Ac}')
-    # print(f'obs.Ab = \n{obs.Ab}')
-    # print(f'obs.b = {obs.b.T}')
+    car_down = car_down_1
+    car_right = zono_op.union_hz_hz_v2(car_right_1, car_right_2)
+
+
+    car_down  = zono_op.cz_to_hz( zono_op.oa_cz_to_hypercube_tight_4d( zono_op.oa_hz_to_cz(car_down)))      # Simplify car down
+    car_right = zono_op.cz_to_hz( zono_op.oa_cz_to_hypercube_tight_4d( zono_op.oa_hz_to_cz(car_right)))     # Simplify car right
+
+
+    obs = zono_op.union_hz_hz_v2(car_down, car_right)
+    car_vis = obs
+
+
+
 
 
     print(f'Obs. : ng = {obs.ng}\t nc = {obs.nc}\t nb = {obs.nb}')
-    print(f'Safe : ng = {safe.ng}\t nc = {safe.nc}\t nb = {safe.nb}')
-
     env.vis_background()
-    # vis.vis_hz([obs], colors = colors, show_edges=True, zorder=100)
-    env.grid = env.vis.vis_hz_brs(hz = obs, brs_settings=env.brs_settings)
+    static_obs_vis = HybridZonotope(car_vis.Gc[0:2, :], car_vis.Gb[0:2, :], car_vis.C[0:2, :], car_vis.Ac, car_vis.Ab, car_vis.b)
+    vis.vis_hz([static_obs_vis], colors = obs_color, show_edges=True, zorder=100)
+    # env.grid = env.vis.vis_hz_brs(hz = static_obs_vis, brs_settings=env.brs_settings)
     plt.show()
-
-    env.vis.ax.set_title(f'ng = {obs.ng}, nc = {obs.nc}, nb = {obs.nb}', fontsize=16)
-    name = f'brs_N_{i}'
-    env.vis.fig.savefig(f'./results/dynamic2/reduced_brs/{name}.pdf', dpi=300)
+    env.vis.ax.set_title(f'road = {road}, iter = {i}, states: position, ng = {obs.ng}, nc = {obs.nc}, nb = {obs.nb}', fontsize=16)
+    name = f'frs_N_{i}'
+    env.vis.fig.savefig(f'./results/dynamic/{road}/{name}_pos.pdf', dpi=300)
     plt.close(env.vis.fig)
     env.vis.new_fig()
+    #
+    env.vis_background()
+    static_obs_vis = HybridZonotope(car_vis.Gc[2:, :], car_vis.Gb[2:, :], car_vis.C[2:, :], car_vis.Ac, car_vis.Ab, car_vis.b)
+    vis.vis_hz([static_obs_vis], colors = obs_color, show_edges=True, zorder=100)
+    # env.grid = env.vis.vis_hz_brs(hz = static_obs_vis, brs_settings=env.brs_settings)
+    plt.show()
+    env.vis.ax.set_title(f'road = {road}, iter = {i}, states: velocity, ng = {obs.ng}, nc = {obs.nc}, nb = {obs.nb}', fontsize=16)
+    name = f'frs_N_{i}'
+    env.vis.fig.savefig(f'./results/dynamic/{road}/{name}_vel.pdf', dpi=300)    
+    plt.close(env.vis.fig)
+    env.vis.new_fig()
+
+
+
+
+
+
+
+
 
 

src/tests/test_operations.py
@@ -10,9 +10,9 @@ from utils.visualization import ZonoVisualizer
 from utils.operations.operations import ZonoOperations
 from utils.sets.hybrid_zonotopes import HybridZonotope
 from utils.sets.constrained_zonotopes import ConstrainedZonotope
-from utils.environments.static_env1 import StaticEnv1
-from utils.ego_model_2d import DynamicsModel
-from utils.environments.static_env1 import StaticEnv1, ParamBRS
+from utils.ego_model_4d import DynamicsModel
+from utils.environments.dynamic_env_temp import DynamicEnv
+from utils.environments.dynamic_env_temp import StateSpaceSafe, DynamicObstacleSpace
 
 
 
@@ -23,6 +23,15 @@ This test script tests the methods:
     oa_constr_hz_scott
 '''
 
+options = 'outer'
+reduction_options = 'reduced'
+
+# Initialize objects
+dynamics = DynamicsModel()
+zono_op = ZonoOperations()
+vis = ZonoVisualizer(zono_op = zono_op)
+env = DynamicEnv(zono_op = zono_op, dynamics = dynamics, visualizer = vis, options = options)
+
 
 colors = [
     (0.949, 0.262, 0.227, 0.6),     # Obstacle (Red)
@@ -30,51 +39,70 @@ colors = [
     (0.231, 0.780, 0.160, 1.0),     # Parking spot (Green)
     (0.423, 0.556, 0.749, 0.5)      # BRS (Blue)
 ]
-zono_op = ZonoOperations()
-dynamics = DynamicsModel()
-vis = ZonoVisualizer(zono_op)
-brs_plot_params = ParamBRS(dynamics = dynamics, space = 'outer')
-env = StaticEnv1(zono_op = zono_op, dynamics = DynamicsModel(), visualizer = vis)
-vis.brs_plot_settings(brs_plot_params)
-vis.ax.set_xlim(-7.0, 7.0); vis.ax.set_ylim(-12, 14)
-vis.ax.spines['right'].set_visible(True); vis.ax.spines['left'].set_visible(True)
-vis.ax.spines['top'].set_visible(True); vis.ax.spines['bottom'].set_visible(True)
-vis.ax.get_xaxis().set_visible(True); vis.ax.get_yaxis().set_visible(True)
-vis.ax.grid(True)
-
-##############################################################################
-#                              Original Sets                                 #
-##############################################################################
-hz1 = SamplesHZ().set_a; hz2 = SamplesHZ().set_b
-hz3 = SamplesHZ().set_c; hz4 = SamplesHZ().set_d
-# hz = zono_op.union_hz_hz(hz1, hz2)
-# hz = zono_op.union_hz_hz(hz, hz3)
-# cz = zono_op.oa_hz_to_cz(hz)
-
-cz1 = SamplesCZ().set_1; cz2 = SamplesCZ().set_2; cz3 = SamplesCZ().set_3; cz4 = SamplesCZ().set_4
-cz_original = zono_op.intersection_cz_cz(cz1, cz2)
-cz_original = zono_op.intersection_cz_cz(cz_original, cz3)
-cz_original = zono_op.intersection_cz_cz(cz_original, cz4)
-hz_original = zono_op.cz_to_hz(cz_original)
-start_time = time.perf_counter()    
+obs_color = (0.949, 0.262, 0.227, 0.6)
+road_color = (0.717, 0.694, 0.682, 0.5)
+blue_color = (0.423, 0.556, 0.749, 0.5)
 
 
-# cz = zono_op.oa_cz_new(cz_original)
-cz = zono_op.oa_cz_to_hypercube_tight(cz_original)
 
 
-end_time = time.perf_counter()
-hz = zono_op.cz_to_hz(cz)
-print(f'time = {end_time - start_time}')
-print(f'ng_original = {hz_original.ng}, nc_original = {hz_original.nc}, nb_original = {hz_original.nb}')
-print(f'ng = {hz.ng}, nc = {hz.nc}, nb = {hz.nb}')
 
-vis.vis_hz([hz], colors = colors, show_edges=True)
-# vis.vis_hz([hz_original], colors = colors, show_edges=True)
-# vis.vis_hz([c_oa_cz], colors = colors, show_edges=True)
+
+
+### Road
+road_d = StateSpaceSafe().road_down
+road_r = StateSpaceSafe().road_right
+road = zono_op.union_hz_hz_v2(road_d, road_r)
+# road = road_d
+
+
+
+### Obstacles
+car_d = DynamicObstacleSpace().car_outer_d
+#
+A = np.array([
+    [1.0, 0.0],
+    [0.0, 1.0]
+])
+B = np.array([
+    [0.5, 0.0],
+    [0.0, 0.5]
+])
+W = HybridZonotope(np.zeros((2, 2)), np.zeros((2, 0)), np.zeros((2, 1)), np.zeros((0, 2)), np.zeros((0, 0)), np.zeros((0, 1)))
+
+# car_d = zono_op.one_step_frs_hz(X = road, U = env.input_space, I = car_d, A = A, B = B, W = W)
+# car_d = zono_op.one_step_frs_hz(X = road, U = env.input_space, I = car_d, A = A, B = B, W = W)
+# car_d = zono_op.one_step_frs_hz(X = road, U = env.input_space, I = car_d, A = A, B = B, W = W)
+# car_d = zono_op.one_step_frs_hz(X = road, U = env.input_space, I = car_d, A = A, B = B, W = W)
+# car_d = zono_op.one_step_frs_hz(X = road, U = env.input_space, I = car_d, A = A, B = B, W = W)
+# car_d = zono_op.one_step_frs_hz(X = road, U = env.input_space, I = car_d, A = A, B = B, W = W)
+# car_d = zono_op.one_step_frs_hz(X = road, U = env.input_space, I = car_d, A = A, B = B, W = W)
+# car_d = zono_op.one_step_frs_hz(X = road, U = env.input_space, I = car_d, A = A, B = B, W = W)
+# car_d = zono_op.one_step_frs_hz(X = road, U = env.input_space, I = car_d, A = A, B = B, W = W)
+# car_d = zono_op.one_step_frs_hz(X = road, U = env.input_space, I = car_d, A = A, B = B, W = W)
+# car_d = zono_op.one_step_frs_hz(X = road, U = env.input_space, I = car_d, A = A, B = B, W = W)
+
+#
+cz = zono_op.oa_hz_to_cz(car_d)
+cz = zono_op.oa_cz_to_hypercube_tight(cz)
+car_d = zono_op.cz_to_hz(cz)
+# car_d = zono_op.intersection_hz_hz(car_d, road)
+
+
+### Intersection
+# inters = zono_op.intersection_hz_hz(car_d, road)
+
+
+
+### Plot
+env.vis_background()
+vis.vis_hz([road], colors = road_color, show_edges=True, zorder=100)
+vis.vis_hz([car_d], colors = obs_color, show_edges=True, zorder=101)
+# vis.vis_hz([inters], colors = blue_color, show_edges=True, zorder=101)
+
+
 plt.show()
-
-
+env.vis.fig.savefig(f'./tests/plots/0.pdf', dpi=300)
 
 
 

src/utils/ego_model_4d.py
@@ -19,8 +19,7 @@ class DynamicsModel:
         ])
         self.v_min = -self.v_max
 
-        a_max = 5.0 # v1
-        a_max = 1.0 # v2
+        a_max = 2.0 # v2
 
         self.a_max = np.array([
             a_max,    # x - acceleration

src/utils/operations/operations.py
@@ -286,7 +286,7 @@ class ZonoOperations:
         See the work in [1] for details
         '''
         assert cz1.dim == cz2.dim, f'Both constrained zonotopes must have the same dimensionality, \
-                                    whereas cz1.dimension = {cz1.dimension} and cz2.dimension = {cz2.dimension}.'
+                                    whereas cz1.dimension = {cz1.dim} and cz2.dimension = {cz2.dim}.'
         
         C = cz1.C
         G = np.block([cz1.G, np.zeros((cz1.G.shape[0], cz2.G.shape[1]))]) # TODO
@@ -989,20 +989,17 @@ class ZonoOperations:
         '''
 
         bounds = np.array([
-            [-2.5, 2.5],        # Bounds in x-direction
-            [-2.5, 2.5]         # Bounds in y-direction
+            [-2.5, 2.0],        # Bounds in x-direction
+            [-1.45, 1.45]         # Bounds in y-direction
         ])
 
-        # Step 0: Define Parameters
+        # Define Parameters
         step_size = 0.05
         min_val = np.zeros((cz.dim, 1))         # To store the minimum value of each dimension
         max_val = np.zeros((cz.dim, 1))         # To store the maximum value of each dimension
         c_new   = np.zeros((cz.dim, 1))         # To store the new center
         G_new   = np.zeros((cz.dim, cz.dim))    # To store the new generators
-
-        # hypercube = self.oa_lifted_cz_to_hypercube(cz) # Compute the unconstrained hypercube
-        # hypercube.C = np.zeros((hypercube.dim, 1))
-
+        
         G_hyper = np.array([
             [2.5, 0.0],
             [0.0, 2.5]
@@ -1021,20 +1018,22 @@ class ZonoOperations:
 
             hypercube.G[d, d] = step_size
 
-            n_steps = int((bounds[d, 1] - bounds[d, 0]))
+            # n_steps = int((bounds[d, 1] - bounds[d, 0]))
             # Loop through the positive axis 
             val = bounds[d, 0]
-            # for n in range(-int(n_steps/2), int(n_steps/2)):
-            while True and val < n_steps/2:
+            # while True and val < n_steps/2:
+            while True and val < bounds[d, 1]:
                 val += step_size
                 hypercube.C[d, 0] = val
                 inters = self.intersection_cz_cz(cz, hypercube)
                 if ctr == 0:
                     if not self.is_empty_cz(inters):
-                        min_val[d, 0] = val# + step_size
+                        # min_val[d, 0] = val# + step_size
+                        min_val[d, 0] = val + step_size
                         ctr += 1
                 elif ctr == 1:
                     if self.is_empty_cz(inters):
+                        # max_val[d, 0] = val - 2*step_size
                         max_val[d, 0] = val - 2*step_size
                         break
 
@@ -1044,11 +1043,117 @@ class ZonoOperations:
 
             # Compute the new center as the middle point of each dimension
             c_new[d, 0] = (max_val[d, 0] + min_val[d, 0]) / 2
-            # G_new[d, d] = (abs(max_val[d, 0]) - abs(min_val[d, 0])) / 2
             G_new[d, d] = (max_val[d, 0] - min_val[d, 0]) / 2
+
+        print(f'max_val = {max_val.T}')
+        print(f'min_val = {min_val.T}')
 
         return ConstrainedZonotope(G_new, c_new, np.zeros((0, G_new.shape[1])), np.zeros((0, 1)))
 
+    def oa_cz_to_hypercube_tight_4d(self, cz: ConstrainedZonotope) -> ConstrainedZonotope:
+        '''
+        This method is specifically designed for constrained zonotopes that represent very simple sets, however with a lot of redundancy 
+
+        To set the theme, this is gonna be used for the FRS of moving obstacles in the environment.
+        More precisely, given that the obstacle is a moving car and we initially represent it as a simple rectangle, we then wanna compute
+        its forward reachable set and the RCI for each FRS. Then we wanna compute its complement to see which set of states are actually safe
+        for the ego vehicle. Since we only have at our disposal a formula for the computation of the complement of a constrained zonotope
+        but not for a hybrid zonotope, we wanna convert that hybrid zonotope into a constrained zonotope.
+
+        Generally, this is gonna result in an over-approximation. However, even that over-approximation contains a lot of redundant information
+        because all we do (at the moment) to generate that constrained zonotope is to simply use the method 'oa_hz_to_cz'.
+
+        Therefore, we need to take extra steps to reduce the complexity of that CZ. This method does exactly that, that is it takes care of that
+        second step of over-approximation to significantly reduce the complexity of the CZ. The goal will be to generate a hypercube over-approximation        
+        
+        In this version we:
+
+        Assume that the dimensionality of the space is 'n'
+        Define a hypercube of dimensionality 'n' as a zonotope where each side of the hypercube is equal to the max and min of the
+        constrained zonotope when all of its constraints are ignored. This applies for all dimensions apart from the one that will be examined at each time.
+        For the dimension that is examined just define its size to be the same with the step size (This is gonna be clearer later on)
+        
+        Then for each dimension we compute the intersection of the original cosntrained zonotope with the hypercube with its center shifted by a single step.
+
+        Then save the value of the center of the hypercube in that dimension
+
+        These values are the min and max values in each dimensions of the constrained zonotope.
+        
+        We can use these min/max values to define a tight hypercube over-approximation of the constrained zonotope
+        '''
+
+        bounds = np.array([
+            [-2.5, 2.1],        # Bounds on position in x-direction
+            [-1.5, 1.5],      # Bounds on position in y-direction
+            [-1.1, 1.1],        # Bounds on velocity in x-direction
+            [-1.1, 1.1]         # Bounds on velocity in y-direction 
+        ])
+
+        # Define Parameters
+        step_size = 0.01
+        min_val = np.zeros((cz.dim, 1))         # To store the minimum value of each dimension
+        max_val = np.zeros((cz.dim, 1))         # To store the maximum value of each dimension
+        c_new   = np.zeros((cz.dim, 1))         # To store the new center
+        G_new   = np.zeros((cz.dim, cz.dim))    # To store the new generators
+        
+        G_hyper = np.array([
+            [3.0, 0.0, 0.0, 0.0],
+            [0.0, 3.0, 0.0, 0.0],
+            [0.0, 0.0, 2.0, 0.0],
+            [0.0, 0.0, 0.0, 2.0]
+        ])
+        C_hyper = np.array([
+            [0.0],
+            [0.0],
+            [0.0],
+            [0.0]
+        ])
+
+        hypercube = ConstrainedZonotope(G_hyper, C_hyper, np.zeros((0, 4)), np.zeros((0, 1)))
+
+        for d in range(cz.dim):
+            # Find maximum vertical value of dimension d
+            ctr = 0
+            original_G = hypercube.G[d, d]
+
+            hypercube.G[d, d] = step_size
+
+            # n_steps = int((bounds[d, 1] - bounds[d, 0]))
+            # Loop through the positive axis 
+            val = bounds[d, 0]
+            # while True and val < n_steps/2:
+            while True and val < bounds[d, 1]:
+                val += step_size
+                hypercube.C[d, 0] = val
+                inters = self.intersection_cz_cz(cz, hypercube)
+                if ctr == 0:
+                    if not self.is_empty_cz(inters):
+                        # min_val[d, 0] = val# + step_size
+                        min_val[d, 0] = val + step_size
+                        ctr += 1
+                elif ctr == 1:
+                    if self.is_empty_cz(inters):
+                        # max_val[d, 0] = val - 2*step_size
+                        max_val[d, 0] = val - 2*step_size
+                        break
+
+            # Reset G, C
+            hypercube.G[d, d] = original_G
+            hypercube.C[d, 0] = 0
+
+            # Compute the new center as the middle point of each dimension
+            c_new[d, 0] = (max_val[d, 0] + min_val[d, 0]) / 2
+            G_new[d, d] = (max_val[d, 0] - min_val[d, 0]) / 2
+
+        print(f'x :\t[{min_val[0, 0]:+0,.2f} -> {max_val[0, 0]:+0,.2f}]')
+        print(f'y :\t[{min_val[1, 0]:+0,.2f} -> {max_val[1, 0]:+0,.2f}]')
+        print(f'vx:\t[{min_val[2, 0]:+0,.2f} -> {max_val[2, 0]:+0,.2f}]')
+        print(f'vy:\t[{min_val[3, 0]:+0,.2f} -> {max_val[3, 0]:+0,.2f}]')
+
+        # print(f'max_val = {max_val.T}')
+        # print(f'min_val = {min_val.T}')
+
+        return ConstrainedZonotope(G_new, c_new, np.zeros((0, G_new.shape[1])), np.zeros((0, 1)))
 
     ############################################################################################################
     # Hybrid Zonotope methods

src/utils/visualization.py
@@ -43,18 +43,30 @@ class ZonoVisualizer:
         self.fig, self.ax = plt.subplots()
         self.manager = plt.get_current_fig_manager()
         self.manager.window.attributes('-zoomed', True)
-        self.ax.spines['right'].set_visible(False); self.ax.spines['left'].set_visible(False)
-        self.ax.spines['top'].set_visible(False); self.ax.spines['bottom'].set_visible(False)
-        self.ax.get_xaxis().set_visible(False); self.ax.get_yaxis().set_visible(False)
+
+        # TODO: TEMPORARY
+        # self.ax.spines['right'].set_visible(False); self.ax.spines['left'].set_visible(False)
+        # self.ax.spines['top'].set_visible(False); self.ax.spines['bottom'].set_visible(False)
+        # self.ax.get_xaxis().set_visible(False); self.ax.get_yaxis().set_visible(False)
+        self.ax.set_xticks(np.arange(-2.5, 2.5, 0.1)); self.ax.set_yticks(np.arange(-1.4, 1.4, 0.1))    # Set ticks every 0.1 and 0.1
+        self.ax.grid(True, which='both', axis='both', linestyle='--', color='gray', linewidth=0.5)      # Add grid
+        self.ax.tick_params(axis='both', which='major', labelsize=6)                                    # Reduce font size of tick numbers
+
         self.ax.set_xlim(-2.5, 2.5); self.ax.set_ylim(-1.4, 1.4)
 
     def new_fig(self):
         self.fig, self.ax = plt.subplots()
         self.manager = plt.get_current_fig_manager()
         self.manager.window.attributes('-zoomed', True)
-        self.ax.spines['right'].set_visible(False); self.ax.spines['left'].set_visible(False)
-        self.ax.spines['top'].set_visible(False); self.ax.spines['bottom'].set_visible(False)
-        self.ax.get_xaxis().set_visible(False); self.ax.get_yaxis().set_visible(False)
+        
+        # TODO: TEMPORARY
+        # self.ax.spines['right'].set_visible(False); self.ax.spines['left'].set_visible(False)
+        # self.ax.spines['top'].set_visible(False); self.ax.spines['bottom'].set_visible(False)
+        # self.ax.get_xaxis().set_visible(False); self.ax.get_yaxis().set_visible(False)
+        self.ax.set_xticks(np.arange(-2.5, 2.5, 0.1)); self.ax.set_yticks(np.arange(-1.4, 1.4, 0.1))    # Set ticks every 0.1 and 0.1
+        self.ax.grid(True, which='both', axis='both', linestyle='--', color='gray', linewidth=0.5)      # Add grid
+        self.ax.tick_params(axis='both', which='major', labelsize=6)                                    # Reduce font size of tick numbers
+
         self.ax.set_xlim(-2.5, 2.5); self.ax.set_ylim(-1.4, 1.4)
 
     def save(self, path: str) -> None:
@@ -117,7 +129,8 @@ class ZonoVisualizer:
 
             # Fill the interior of the Polytope
             zorder = 1 if zorder is None else zorder
-            color = [0.423, 0.556, 0.749, 1.0]
+            color = colors
+            # color = [0.423, 0.556, 0.749, 1.0]
 
             poly = Polygon(vertices, closed = True, fill = True, facecolor = (color[0], color[1], color[2]),  alpha = color[3], zorder = zorder)
             self.ax.add_patch(poly)
@@ -146,7 +159,8 @@ class ZonoVisualizer:
                 b = b.reshape(-1, 1)
                 cz.append(ConstrainedZonotope(hz.Gc, hz.C + hz.Gb @ b, hz.Ac, hz.b - hz.Ab @ b))
  
-            self.vis_cz(cz, colors = colors[i], zorder = zorder, show_edges = show_edges)
+            # self.vis_cz(cz, colors = colors[i], zorder = zorder, show_edges = show_edges)
+            self.vis_cz(cz, colors = colors, zorder = zorder, show_edges = show_edges)
             i += 1
 
     def vis_hz_brs(self, hz, brs_settings):